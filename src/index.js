a8149e6fea43d6ab61c00c21373287ea27f9433d
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-01-05 19:14:51 -0400

    feat: add more info in errors and add spinner (closes #2)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const io = require('socket.io-client');
 const fetch = require('node-fetch-commonjs');
+const owy = require('owy');
 const FormData = require('form-data');
 const form = new FormData();
+const spinner = new owy.Spinner();
 
 const filePath = process.argv[2];
 const fileStream = fs.createReadStream(filePath);
@@ -21,15 +23,21 @@ const options = {
     }
 }
 
+spinner.start('Uploading replay...');
 const res = fetch('https://ordr-api.issou.best/renders', { ...options })
 
 .then(async res => {
-	if(res.status !== 201) {
-		console.log(`Error: ${res.status}`);
+	if(res.status === 429) {
+		spinner.fail('Replay has been uploaded recently. Please try again later.');
 		process.exit(1);
 	}
+
 	const resp = await res.json();
-	console.log('Replay uploaded! Render will start in a few seconds.')
+	if(res.status !== 201) {
+		spinner.fail(`Error: ${resp.message} (${res.status})`);
+		process.exit(1);
+	}
+	spinner.success('Replay uploaded! Render will start in a few seconds.')
 
 	const socket = io('https://ordr-ws.issou.best');
 	socket.on('render_progress_json', renderInfo => {
