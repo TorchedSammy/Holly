db72231526d0b9caa43a802f50ddf7932d0e60d9
devins2518 <drsingh2518@icloud.com>
2022-01-14 19:56:15 -0600

    refactor: cleanup; move spinner clones near respective closures (#3)
    * style: enforce tabs with rustfmt
    
    * refactor: consolidate spinner clones towards closures

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,44 @@
 mod holly;
 
-use std::{env, time, thread::sleep};
-use reqwest::blocking::multipart;
-use rust_socketio::{ClientBuilder, Payload, Client};
-use std::io::Read;
 use indicatif::ProgressBar;
+use reqwest::blocking::multipart;
+use rust_socketio::{Client, ClientBuilder, Payload};
+use std::io::Read;
+use std::{env, thread::sleep, time};
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	let spinner = ProgressBar::new_spinner(); // thanks indicatif this makes sense
 	spinner.enable_steady_tick(80);
-	// tfw you rust
-	let spinner1 = spinner.clone();
-	let spinner2 = spinner.clone();
-	let spinner3 = spinner.clone();
 
 	let form = multipart::Form::new()
 		.text("skin", "blueberry_v1_7_0")
 		.text("resolution", "1280x720")
 		.text("username", "Holly")
-		.file("replayFile", &args[1]).unwrap();
+		.file("replayFile", &args[1])
+		.unwrap();
 
 	spinner.set_message("Uploading replay...");
 	let client = reqwest::blocking::Client::new();
-	let mut res = client.post("https://ordr-api.issou.best/renders")
+	let mut res = client
+		.post("https://ordr-api.issou.best/renders")
 		.multipart(form)
-		.send().unwrap();
+		.send()
+		.unwrap();
 	spinner.set_message("Replay uploaded, waiting for response");
 
 	// check if status code is 429
 	if res.status() == reqwest::StatusCode::TOO_MANY_REQUESTS {
 		// get X-RateLimit-Reset header
 		let now = time::Instant::now();
-		let stamp = res.headers().get("X-RateLimit-Reset").unwrap().to_str().unwrap().parse::<u64>().unwrap();
+		let stamp = res
+			.headers()
+			.get("X-RateLimit-Reset")
+			.unwrap()
+			.to_str()
+			.unwrap()
+			.parse::<u64>()
+			.unwrap();
 		let duration = time::Duration::from_secs(stamp) - now.elapsed();
 
 		println!("{}", duration.as_secs());
@@ -54,47 +60,59 @@ fn main() {
 	spinner.set_message("Uploaded successfully, waiting for render to start");
 	let render_id = r.renderID;
 
-	let done_callback = move |payload: Payload, socket: Client| {
-		let data = match payload {
-			Payload::String(s) => s,
-			_ => "".to_string(),
-		};
+	let done_callback = {
+		let spinner = spinner.clone();
+		move |payload: Payload, socket: Client| {
+			let render_id = render_id;
+			let data = match payload {
+				Payload::String(s) => s,
+				_ => "".to_string(),
+			};
 
-		let p: holly::RenderDone = serde_json::from_str(&data).unwrap();
-		if p.renderID == render_id {
-			spinner1.set_message(p.videoUrl);
-			spinner1.finish();
-			socket.disconnect();
-			std::process::exit(0);
+			let p: holly::RenderDone = serde_json::from_str(&data).unwrap();
+			if p.renderID == render_id {
+				spinner.set_message(p.videoUrl);
+				spinner.finish();
+				socket.disconnect();
+				std::process::exit(0);
+			}
 		}
 	};
 
-	let progress_callback = move |payload: Payload, _: Client| {
-		let data = match payload {
-			Payload::String(s) => s,
-			_ => "".to_string(),
-		};
-		let p: holly::RenderProgress = serde_json::from_str(&data).unwrap();
-		if p.renderID == render_id {
-			spinner2.set_message(p.progress);
+	let progress_callback = {
+		let spinner = spinner.clone();
+		move |payload: Payload, _: Client| {
+			let data = match payload {
+				Payload::String(s) => s,
+				_ => "".to_string(),
+			};
+			let p: holly::RenderProgress = serde_json::from_str(&data).unwrap();
+			if p.renderID == render_id {
+				spinner.set_message(p.progress);
+			}
 		}
 	};
 
-	let failed_callback = move |payload: Payload, socket: Client| {
-		let data = match payload {
-			Payload::String(s) => s,
-			_ => "".to_string(),
-		};
+	let failed_callback = {
+		let spinner = spinner.clone();
+		move |payload: Payload, socket: Client| {
+			let data = match payload {
+				Payload::String(s) => s,
+				_ => "".to_string(),
+			};
 
-		let p: holly::RenderFailed = serde_json::from_str(&data).unwrap();
-		if p.renderID == render_id {
-			spinner3.set_message(format!("Render failed. Error: {} ({})", p.errorMessage, p.errorCode));
-			spinner3.finish();
-			socket.disconnect();
-			std::process::exit(0);
+			let p: holly::RenderFailed = serde_json::from_str(&data).unwrap();
+			if p.renderID == render_id {
+				spinner.set_message(format!(
+					"Render failed. Error: {} ({})",
+					p.errorMessage, p.errorCode
+				));
+				spinner.finish();
+				socket.disconnect();
+				std::process::exit(0);
+			}
 		}
 	};
-
 	ClientBuilder::new("https://ordr-ws.issou.best")
 		.on("render_progress_json", progress_callback)
 		.on("render_done_json", done_callback)
